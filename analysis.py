9352043140f73ac9bbbbca4894946f2d3f79143e
N0T10N <78817829+N0t10n@users.noreply.github.com>
2021-04-12 15:35:24 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -9,28 +9,21 @@ import numpy as np
 import pandas as pd
 
 # Functions
-def open_file(file):
-    return pd.read_csv(file)
-
-def groupby(df):
-    return df.groupby(['original_publish_year'])
-
-def best_book(df, author):
-    h_rate = np.max(df['minmax_norm_ratings'])
-    return 
-
-
 def main(file, author):
-    df = open_file(file) # Create DataFrame
-    group = groupby(df)
-    
-    return df, group
+    # Creates a DataFrame
+    df = pd.read_csv(file)
+    # Creates the group out of the years
+    group = df.groupby(['original_publish_year'])
+    # Finding the mean of minmax_norm_ratings
+    means = df.groupby('minmax_norm_ratings').mean()
+    # Returns the book with higher score of an author
+    book = df.loc[df['minmax_norm_ratings'] == np.max(df['minmax_norm_ratings']), author]
+    return df, group, means, book
 
+A = {
+    'ratings': [1, 3, 4, 2, 5, 3],
+    'original_publish_year': [2018, 2016, 2010, 2012, 2019, 2000],
+    'minmax_norm_ratings': [.3, .4, .8, .2, .1, .6]
+}
 
-# min_max_norm_ratings means
-means = df.groupby('minmax_norm_ratings').mean()
-
-
-a = groupby(df, ['original_publish_year'])
-
-print(a)
+data = pd.DataFrame(A)
