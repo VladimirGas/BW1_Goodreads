d388e7c608c20f745d4297dd24f87b37ce80bffe
N0T10N <78817829+N0t10n@users.noreply.github.com>
2021-04-12 15:58:40 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 12 11:59:41 2021
 
-@author: vladimirgasanov
-"""
 import numpy as np
 import pandas as pd
 
 # Functions
-def main(file, author):
+def analysis(file, author):
     # Creates a DataFrame
     df = pd.read_csv(file)
     # Creates the group out of the years
@@ -18,12 +12,12 @@ def main(file, author):
     means = df.groupby('minmax_norm_ratings').mean()
     # Returns the book with higher score of an author
     book = df.loc[df['minmax_norm_ratings'] == np.max(df['minmax_norm_ratings']), author]
+    
     return df, group, means, book
 
-A = {
-    'ratings': [1, 3, 4, 2, 5, 3],
-    'original_publish_year': [2018, 2016, 2010, 2012, 2019, 2000],
-    'minmax_norm_ratings': [.3, .4, .8, .2, .1, .6]
-}
-
-data = pd.DataFrame(A)
+if __name__ == "__main__":
+    analysis()
+else:
+    print(
+        'Call the analysis function with a csv file and an author as the corresponding arguments'
+    )
