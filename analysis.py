292bc7130f60692d4e2117fad961a58da875e619
N0T10N <78817829+N0t10n@users.noreply.github.com>
2021-04-12 13:00:43 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -7,15 +7,9 @@ Created on Mon Apr 12 11:59:41 2021
 """
 import numpy as np
 import pandas as pd
+import csv
 
-a = np.random.randint(10, size = 100)
-b = np.random.randint(10, size = 100)
-
-a_list = {'a': a, 'b':b }
-
-with open('file.py', 'w') as file:
-    file.write(a)
-
+# Necessary functions
 def open_file(file):
     with open(file, 'r') as csv:
         data = csv.read()
